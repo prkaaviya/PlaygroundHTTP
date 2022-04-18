287b40aa6acd7382ecbe69577aabbdbcb295b006
Kaaviya Ramkumar <65661406+prkaaviya@users.noreply.github.com>
2022-04-18 12:53:43 +0530

    Add HTTPS config changes in Docker

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,22 @@ RUN apt-get install -y vim && apt-get install -y lynx && \
     apt-get clean 
 
 COPY ./conf/mysite.conf /etc/apache2/sites-available/
+COPY ./conf/mysite-ssl.conf /etc/apache2/sites-available/
+
 RUN mkdir /var/www/html/mysite
 COPY ./html/index.html /var/www/html/mysite
 
+COPY ./ssl/key.pem /etc/ssl/private
+COPY ./ssl/cert.pem /etc/ssl/certs
+
 RUN a2dissite 000-default.conf
 RUN a2ensite mysite.conf
+RUN a2ensite mysite-ssl.conf
+RUN a2enmod proxy_http
 RUN a2enmod headers
+RUN a2enmod ssl
 
-EXPOSE 8080
+EXPOSE 80
+EXPOSE 443
 
 CMD ["apachectl", "-D", "FOREGROUND"]
--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,20 @@
+# Set top directory 
+PROJECT_DIR=`pwd`
+SSL_DIR="$PROJECT_DIR/ssl"
+KEY_FILE="$SSL_DIR/key.pem"
+CERT_FILE="$SSL_DIR/cert.pem"
+
+# Make sure to give proper permissions to .env file to be executed in the container
+chmod 764 .env
+
+# Install mkcert utility to create and install certs
+brew list mkcert || brew install mkcert
+
+rm -f $KEY_FILE $CERT_FILE || true
+
+mkcert -client -key-file $KEY_FILE -cert-file $CERT_FILE mysite.com "*.mysite.com" mysite.test localhost 127.0.0.1 ::1
+mkcert -install
+
+# Use docker to build the web server at localhost
 docker-compose build
 docker-compose up
